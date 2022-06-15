678bee407c18713a9ce143bc2282b5dbc16cc05d
Kiril <alexbour82@gmail.com>
2022-06-15 20:52:01 -0300

    inital connection to mongodb and posts inital creation

--- server/server.js
+++ server/server.js
@@ -1,2 +1,16 @@
 const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT
+const connectDB = require('./config/db')
+
+connectDB()
+
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.listen(port, () => console.log(`server started on port ${port}`))
+
+app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/posts', require('./routes/postRoutes'))
