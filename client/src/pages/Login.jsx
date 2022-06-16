c16aad8b1712ec01a729f5a1b153bdb54d1689be
Kiril <alexbour82@gmail.com>
2022-06-16 19:21:23 -0300

    state handeling of login and register values

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,29 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 import AuthBack from '../static/images/auth-back.jpg'
 import {BiLogIn} from 'react-icons/bi'
 
 function Login() {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  })
+
+  const {username, password} = formData
+  
+  const changeState = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const submit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div className='h-screen w-screen bg-zinc-900 text-white flex flex-row'>
 
       {/* sidebar */}
-      <div className='w-7/12 sm:block hidden'>
+      <div className='w-7/12 lg:block hidden'>
         <img src={AuthBack} alt={'Background for login'} className="w-full h-full p-3 py-6 rounded-2xl drop-shadow-2xl"/>
       </div>
 
       {/* form */}
-      <div className='w-full sm:w-5/12 my-auto'>
+      <div className='w-full lg:w-5/12 my-auto'>
           <h1 className='text-4xl text-center'>Login</h1>
-          <form className='flex flex-col m-5 mx-16 text-black'>
-            <input type='text' placeholder='Username or Email' className='p-2 my-4 rounded-lg'/>
-            <input type='password' placeholder='Password' className='p-2 my-4 rounded-lg'/>
+          <form className='flex flex-col m-5 mx-16 text-black' onSubmit={submit}>
+            <input name='username' value={username} type='text' placeholder='Username or Email' className='p-2 my-4 rounded-lg' onChange={changeState}/>
+            <input name='password' value={password} type='password' placeholder='Password' className='p-2 my-4 rounded-lg' onChange={changeState}/>
             
           </form>
 
           <div className='mx-16 flex mb-16'>
-            <button className='bg-red-500 p-3 px-5 rounded-lg text-white'>
-              <p className='flex'>Login <div className='my-auto'><BiLogIn /></div></p>
+            <button className='bg-red-500 p-3 px-5 rounded-lg text-white' type='submit'>
+              <div className='flex'>Login <div className='my-auto ml-2'><BiLogIn /></div></div>
             </button>
             <Link to={'/register'} className='ml-auto my-auto'>
               Don't have an account?
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AuthBack from '../static/images/auth-back.jpg'
+import { Link } from 'react-router-dom'
+import {IoCreateOutline} from 'react-icons/io5'
 
 function Register() {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  })
+
+  const {username, email, password, confirmPassword} = formData
+  
+  const changeState = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const submit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Register</div>
+    <div className='h-screen w-screen bg-zinc-900 text-white flex flex-row'>
+
+      {/* sidebar */}
+      <div className='w-7/12 lg:block hidden'>
+        <img src={AuthBack} alt={'Background for login'} className="w-full h-full p-3 py-6 rounded-2xl drop-shadow-2xl"/>
+      </div>
+
+      {/* form */}
+        <div className='w-full lg:w-5/12 my-auto'>
+            <h1 className='text-4xl text-center'>Register</h1>
+            <form className='flex flex-col m-5 mx-16 text-black' onSubmit={submit}>
+              <input value={username} type='text' placeholder='Username' name='username' className='p-2 my-4 rounded-lg' onChange={changeState}/>
+
+              <label htmlFor='email' className='text-sm text-gray-100'>Email is optional, and we will never share your email with anyone</label>
+              <input value={email} type='text' placeholder='Email' name='email' className='p-2 my-4 rounded-lg' onChange={changeState}/>
+                
+
+              <input value={password} type='password' placeholder='Password' name='password' className='p-2 my-4 rounded-lg' onChange={changeState}/>
+              <input value={confirmPassword} type='password' placeholder='Confirm password' name='confirmPassword' className='p-2 my-4 rounded-lg' onChange={changeState}/>
+              
+            </form>
+
+            <div className='mx-16 flex mb-16'>
+            <button type="submit" className='bg-red-500 p-3 px-5 rounded-lg text-white'>
+              <div className='flex'>Create account!<div className='my-auto ml-2'><IoCreateOutline /></div></div>
+            </button>
+            <Link to={'/'} className='ml-auto my-auto'>
+              Already have an account?
+            </Link>
+
+          </div>
+
+      </div>
+    </div>
   )
 }
 
