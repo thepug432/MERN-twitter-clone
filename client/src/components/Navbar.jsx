35822427031c30c897621274dca1dce962840d70
Kiril <alexbour82@gmail.com>
2022-06-17 15:52:59 -0300

    removed unneeded imports

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux';
 import { logout, reset } from '../features/auth/authSlice';
 import Navbutton from './Navbutton';
