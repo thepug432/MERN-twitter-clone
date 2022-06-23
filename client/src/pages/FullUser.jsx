ebe0d3127894a7ff19dc86a53ddf51674d2e279a
Kiril <alexbour82@gmail.com>
2022-06-23 12:37:37 -0300

    fetching user data for description

--- client/src/pages/FullUser.jsx
+++ client/src/pages/FullUser.jsx
@@ -1,42 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
 import {WiTime2} from 'react-icons/wi'
 import Wrapper from '../components/Wrapper'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
 
 
 function FullUser() {
-  return (
-    <Wrapper>
-        {/* user information */}
-        <div className='text-white flex flex-col border-b border-white p-2'>
-            <div className='flex'>
-                <h1><strong>username</strong></h1>
-                <motion.button 
-                    whileHover={{ scale: 1.1, backgroundColor: "rgb(239, 68, 68)" }} 
-                    whileTap={{ scale: .9 }} 
-                    transition={{ duration: .3 }}
-                    className='ml-auto my-1 mr-1 bg-red-700 p-5 py-1 rounded-lg'
-                >
-                    Follow
-                </motion.button>
-            </div>
-            <p>Description</p>
-            <div>
-                <small className='italic text-gray-400 flex'>
-                    <div className='my-auto'>
-                        <WiTime2/>
-                    </div>
-                    <time>Joined: date</time>
-                </small>
+    const {id} = useParams()
+    const [userData, SetUserData] = useState({
+        username: null,
+        created: null,
+        description: null,
+        followers: null
+    })
+    const [update, setUpdate] = useState(0)
+
+    const forceUpdate = () => {
+        setUpdate(update+1)
+    }
+
+    useEffect(() => {
+        
+        const fetchUserData = async () => {
+            const response = await(await axios.get('/api/userData/usernameFromId', { params: {id: id} })).data
+            SetUserData({
+                username: response.username,
+                created: response.createdAt,
+                description: response.description,
+                followers: response.followers.length,
+            })
+        }
+        fetchUserData()
+
+    }, [update, id])
+
+    return (
+        <Wrapper>
+            {/* user information */}
+            <div className='text-white flex flex-col border-b border-white p-2'>
+                <div className='flex'>
+                    <h1><strong>username</strong></h1>
+                    <motion.button 
+                        whileHover={{ scale: 1.1, backgroundColor: "rgb(239, 68, 68)" }} 
+                        whileTap={{ scale: .9 }} 
+                        transition={{ duration: .3 }}
+                        className='ml-auto my-1 mr-1 bg-red-700 p-5 py-1 rounded-lg'
+                    >
+                        Follow
+                    </motion.button>
+                </div>
+                <p>Description</p>
                 <div>
-                    <small className='text-gray-400'><strong className='text-white'>int</strong> followers</small>
+                    <small className='italic text-gray-400 flex'>
+                        <div className='my-auto'>
+                            <WiTime2/>
+                        </div>
+                        <time>Joined: date</time>
+                    </small>
+                    <div>
+                        <small className='text-gray-400'><strong className='text-white'>int</strong> followers</small>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div>user posts</div>
-    </Wrapper>
-  )
+            <div>user posts</div>
+        </Wrapper>
+    )
 }
 
 export default FullUser
--- server/controllers/UserDataController.js
+++ server/controllers/UserDataController.js
@@ -2,10 +2,9 @@ const asyncHandler = require('express-async-handler')
 const User = require('../model/userModel')
 
 const getUsername = asyncHandler(async (req, res) => {
-    const {username} = await User.findById(req.query.id)
-    res.status(200).json({
-     response: username
-    })
+    const response = await User.findById(req.query.id).select('-password -__v -updatedAt')
+    console.log(response);
+    res.status(200).json(response)
 })
 
 module.exports = {
