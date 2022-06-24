025eaf3c45d06c2723ce550306713372f83c1bb5
Kiril <alexbour82@gmail.com>
2022-06-24 20:19:56 -0300

    added search bar html to explore

--- client/src/pages/Explore.jsx
+++ client/src/pages/Explore.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { motion } from 'framer-motion'
 import Wrapper from '../components/Wrapper'
 
+
 function Explore() {
-  return (
-    <Wrapper>
-        
-    </Wrapper>
-  )
+    const [formData, setFormData] = useState('')
+
+    const changeFormData = (e) => {
+        setFormData(e.target.value)
+    }
+
+    return (
+        <Wrapper>
+            <div className='flex m-3 text-white'>
+                <input value={formData} onChange={changeFormData} type='text' className='bg-zinc-700 w-full rounded-lg px-2'/>
+                <motion.button 
+                    whileHover={{ scale: 1.1, backgroundColor: "rgb(239, 68, 68)" }} 
+                    whileTap={{ scale: .9 }} 
+                    transition={{ duration: .3 }}
+                    className='bg-red-700 px-4 py-3 ml-3 rounded-lg'>
+                Search
+                </motion.button>
+            </div>
+        </Wrapper>
+    )
 }
 
 export default Explore
