ebadf0b77fc188bdc8793951e946b0c3b29dd5ce
Kiril <alexbour82@gmail.com>
2022-06-26 14:54:30 -0300

    added search bar to search

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,38 +1,58 @@
 import React, { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import LoadingPosts from '../components/LoadingPosts'
 import Wrapper from '../components/Wrapper'
+import { motion } from 'framer-motion'
 
 function Search() {
-    const [search, setSearch] = useState(null)
+    const [searchResults, setSearchResults] = useState(null)
+    const [searchValue, setSearchValue] = useState()
     const { query } = useParams()
+    const navigate = useNavigate()
+
+    const change = (e) =>  setSearchValue(e.target.value)
+    const search = () => navigate(`/search/${searchValue}`)
 
 
     return (
         <Wrapper>
+            <div className='flex m-3 text-white'>
+                <input placeholder='Search' value={searchValue} onChange={change} type='text' className='bg-zinc-700 w-full rounded-lg px-2'/>
+                <motion.button 
+                    whileHover={{ scale: 1.1, backgroundColor: "rgb(239, 68, 68)" }} 
+                    whileTap={{ scale: .9 }} 
+                    transition={{ duration: .3 }}
+                    className='bg-red-700 px-4 py-3 ml-3 rounded-lg'
+                    onClick={search}    
+                >
+                Search
+                </motion.button>
+            </div>
+
+            {/* title */}
             <h1 className='text-2xl m-3 text-center'>
                 Your search for <strong className='italic'>{query} </strong> 
                 
-                { search ?
+                { searchResults ?
                     <>returned the following results: </>
                 :
-                    search == null ?
+                    searchResults === 'null' ?
                         <>are loading...</>
                     :
-                        <>no results</>
+                        <>returned no results.</>
                 }
                 
                 
             </h1>
                 
             <div>
-                {search ?
+                {searchResults ?
                     <>posts found</>
                 :
-                    search === null ?
+                    searchResults === 'null' ?
                         <LoadingPosts override='Loading results...' />
                     :
-                    <h1>Nothing found</h1>   
+                    <h1 className='text-center'>Please change your query, and try again.</h1>   
                 }
             </div>
         </Wrapper>
