b2a1f853e315dbe571621ac222a80d40fb37c647
Kiril <alexbour82@gmail.com>
2022-06-26 13:13:43 -0300

    added searching html

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
+import LoadingPosts from '../components/LoadingPosts'
 import Wrapper from '../components/Wrapper'
 
 function Search() {
+    const [search, setSearch] = useState(null)
     const { query } = useParams()
+
 
     return (
         <Wrapper>
-            
+            <h1 className='text-2xl m-3 text-center'>
+                Your search for <strong className='italic'>{query} </strong> 
+                
+                { search ?
+                    <>returned the following results: </>
+                :
+                    search == null ?
+                        <>are loading...</>
+                    :
+                        <>no results</>
+                }
+                
+                
+            </h1>
+                
+            <div>
+                {search ?
+                    <>posts found</>
+                :
+                    search === null ?
+                        <LoadingPosts override='Loading results...' />
+                    :
+                    <h1>Nothing found</h1>   
+                }
+            </div>
         </Wrapper>
     )
 }
