432f27cef46269d06e8926493c6f0a82ea622b83
Kiril <alexbour82@gmail.com>
2022-06-27 20:14:57 -0300

    html changes in settings

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -6,8 +6,12 @@ import Wrapper from '../components/Wrapper'
 
 function Settings() {
     const navigate = useNavigate()
-    const [userData, setUserData] = useState(null)
     const user = useSelector((state) => {return state.auth.user})
+    const [userData, setUserData] = useState({
+        username: null,
+        description: null 
+    })
+    
 
     useEffect(() => {
         if(!user){
@@ -18,8 +22,22 @@ function Settings() {
     return (
         <Wrapper>
             <div className='m-3 flex flex-col'>
-                <input type='text' placeholder={'username'} className='bg-zinc-700 p-3 my-2 rounded-md'/>
-                <textarea type='description' placeholder={'description'} className='bg-zinc-700 p-3 my-2 rounded-md'/>
+
+                <label htmlFor='username'><small className='italic text-gray-300'>Username</small></label>
+                <input 
+                    name='username' 
+                    type='text' 
+                    placeholder={userData.username ? 'Username' : 'Loading...'} 
+                    className='bg-zinc-700 p-3 my-2 rounded-md'
+                />
+
+                <label htmlFor='username'><small className='italic text-gray-300'>Description</small></label>
+                <textarea 
+                    name='description' 
+                    type='description' 
+                    placeholder={userData.description ? 'Description' : 'Loading...'} 
+                    className='bg-zinc-700 p-3 my-2 rounded-md'
+                />
                 <motion.button 
                     whileHover={{ scale: 1.1, backgroundColor: "rgb(239, 68, 68)" }} 
                     whileTap={{ scale: .9 }} 
