715688061224d0a33f407cc28a19e6b26f33fdc0
Kiril <alexbour82@gmail.com>
2022-06-27 10:29:39 -0300

    settings css added

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import { motion } from 'framer-motion'
 import Wrapper from '../components/Wrapper'
 
 function Settings() {
@@ -17,11 +18,16 @@ function Settings() {
     return (
         <Wrapper>
             <div className='m-3 flex flex-col'>
-                <input type='text' placeholder={'username'}/>
-                <input type='description' placeholder={'description'}/>
-                <button>Save</button>
-
-                <button>Logout</button>
+                <input type='text' placeholder={'username'} className='bg-zinc-700 p-3 my-2 rounded-md'/>
+                <textarea type='description' placeholder={'description'} className='bg-zinc-700 p-3 my-2 rounded-md'/>
+                <motion.button 
+                    whileHover={{ scale: 1.1, backgroundColor: "rgb(239, 68, 68)" }} 
+                    whileTap={{ scale: .9 }} 
+                    transition={{ duration: .3 }}
+                    className='bg-red-700 px-6 py-3 my-3 ml-auto rounded-xl'
+                >
+                    Save settings
+                </motion.button>
             </div>
         </Wrapper>
     )
